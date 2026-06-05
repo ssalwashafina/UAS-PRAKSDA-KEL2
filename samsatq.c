8be71049e3e55055d0c312de71e8f8c9289e5422
ssalwashafina <shafinasalwasiti18@gmail.com>
2026-06-05 22:59:11 +0700

    update inisialisasi

--- samsatq.c
+++ samsatq.c
@@ -1,4 +1,3 @@
-
 #include "samsatq.h"
 
 Queue antrianLoket[JUMLAH_LOKET];
@@ -7,27 +6,30 @@ NodeRiwayat *headRiwayat = NULL;
 int nomorAntrianGlobal = 1;
 
 void inisialisasiSistem(void) {
-int i;
+    int i;
+    for (i = 0; i < JUMLAH_LOKET; i++) {
+        antrianLoket[i].front  = NULL;
+        antrianLoket[i].rear   = NULL;
+        antrianLoket[i].jumlah = 0;
+    }
+ 
+    daftarLoket[0].idLoket       = LOKET_PAJAK;
+    strcpy(daftarLoket[0].namaLoket, "Loket Pajak Kendaraan");
+    daftarLoket[0].statusTersedia = 1;
+    daftarLoket[0].totalDilayani  = 0;
+ 
+    daftarLoket[1].idLoket       = LOKET_BALIK_NAMA;
+    strcpy(daftarLoket[1].namaLoket, "Loket Balik Nama");
+    daftarLoket[1].statusTersedia = 1;
+    daftarLoket[1].totalDilayani  = 0;
+ 
+    daftarLoket[2].idLoket       = LOKET_STNK;
+    strcpy(daftarLoket[2].namaLoket, "Loket Penerbitan STNK");
+    daftarLoket[2].statusTersedia = 1;
+    daftarLoket[2].totalDilayani  = 0;
+ 
+    headRiwayat        = NULL;
+    nomorAntrianGlobal = 0;
 
-for(i = 0; i < JUMLAH_LOKET; i++) {
-    antrianLoket[i].front = NULL;
-    antrianLoket[i].rear = NULL;
-    antrianLoket[i].jumlah = 0;
-}
-
-daftarLoket[0].idLoket = LOKET_PAJAK;
-strcpy(daftarLoket[0].namaLoket, "Pajak Kendaraan");
-daftarLoket[0].statusTersedia = 1;
-daftarLoket[0].totalDilayani = 0;
-
-daftarLoket[1].idLoket = LOKET_BALIK_NAMA;
-strcpy(daftarLoket[1].namaLoket, "Balik Nama");
-daftarLoket[1].statusTersedia = 1;
-daftarLoket[1].totalDilayani = 0;
-
-daftarLoket[2].idLoket = LOKET_STNK;
-strcpy(daftarLoket[2].namaLoket, "Penerbitan STNK");
-daftarLoket[2].statusTersedia = 1;
-daftarLoket[2].totalDilayani = 0;
 }
 
