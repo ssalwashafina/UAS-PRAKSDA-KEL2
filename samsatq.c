cca45d18d28002f6e2ba340ad62e37bbdc8a5e43
ssalwashafina <shafinasalwasiti18@gmail.com>
2026-06-05 22:02:40 +0700

    tambah variabel global

---
+++ samsatq.c
@@ -0,0 +1,10 @@
+#include "samsatq.h"
+
+Queue antrianLoket[JUMLAH_LOKET];
+Loket daftarLoket[JUMLAH_LOKET];
+NodeRiwayat *headRiwayat = NULL;
+int nomorAntrianGlobal = 1;
+
+void inisialisasiSistem(void) {
+    
+}
